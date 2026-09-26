d0bee9647ce593cf3505e63814f7853efbcce2cd
BlaMacfly <165155133+BlaMacfly@users.noreply.github.com>
2026-09-26 13:48:37 +0200

    Core/Spell: fix nopch build

--- src/server/game/Spells/SpellMgr.h
+++ src/server/game/Spells/SpellMgr.h
@@ -53,6 +53,7 @@ struct SpellScalingEntry;
 struct SpellShapeshiftEntry;
 struct SpellTargetRestrictionsEntry;
 struct SpellTotemsEntry;
+enum AuraType;
 
 // only used in code
 enum SpellCategories
