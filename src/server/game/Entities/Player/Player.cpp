1fc742bcbb7d7aabc33826d3a13b7dcaf25fb8bb
BlaMacfly <165155133+BlaMacfly@users.noreply.github.com>
2026-09-26 13:48:21 +0200

    Core/Misc: Moved CanSpeak() method to WorldSession class

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -22461,11 +22461,6 @@ void Player::UpdateSpeakTime()
     m_speakTime = current + sWorld->getIntConfig(CONFIG_CHATFLOOD_MESSAGE_DELAY);
 }
 
-bool Player::CanSpeak() const
-{
-    return  GetSession()->m_muteTime <= time (nullptr);
-}
-
 /*********************************************************/
 /***              LOW LEVEL FUNCTIONS:Notifiers        ***/
 /*********************************************************/
