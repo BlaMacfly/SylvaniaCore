bd67bb2199f6c9e0398b7ece781537a894bb73a4
BlaMacfly <165155133+BlaMacfly@users.noreply.github.com>
2026-09-26 15:00:27 +0200

    Core/Misc: utiliser sWorld->GetGameTime() (pas de sous-systeme GameTime:: chez nous)

--- src/server/game/Server/WorldSession.cpp
+++ src/server/game/Server/WorldSession.cpp
@@ -30,7 +30,6 @@
 #include "CharacterPackets.h"
 #include "ChatPackets.h"
 #include "DatabaseEnv.h"
-#include "GameTime.h"
 #include "Group.h"
 #include "Guild.h"
 #include "GuildMgr.h"
@@ -769,7 +768,7 @@ void WorldSession::SendNotification(uint32 stringId, ...)
 
 bool WorldSession::CanSpeak() const
 {
-    return m_muteTime <= GameTime::GetGameTime();
+    return m_muteTime <= sWorld->GetGameTime();
 }
 
 char const* WorldSession::GetTrinityString(uint32 entry) const
